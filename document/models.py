bff4b6f524462caae1dd665b2d901d3385ad38bc
519389812 <519389812>
2020-11-16 19:46:14 +0800

    16nov20 07

--- document/models.py
+++ document/models.py
@@ -7,7 +7,7 @@ class DocxInit(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="创建人")
     template_name = models.CharField(max_length=30, verbose_name="模板名")
     docx_name = models.CharField(max_length=30, verbose_name="文档名")
-    content = models.TextField(verbose_name="内容")
+    content = models.TextField(max_length=500, verbose_name="内容")
     version = models.IntegerField(default=0, verbose_name="版本号")
     create_datetime = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     edit_datetime = models.DateTimeField(auto_now=True, verbose_name="最新修改时间")
@@ -25,8 +25,8 @@ class ContentStorage(models.Model):
     id = models.TextField(max_length=50, primary_key=True)
     docx = models.ForeignKey(DocxInit, on_delete=models.DO_NOTHING, verbose_name="模板")
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="填写人")
-    content = models.TextField(verbose_name="内容")
-    signature = models.TextField(verbose_name="签名", blank=True)
+    content = models.TextField(max_length=500, verbose_name="内容")
+    signature = models.TextField(max_length=10000, verbose_name="签名", blank=True)
     create_datetime = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     edit_datetime = models.DateTimeField(auto_now=True, verbose_name="最新修改时间")
 
@@ -42,8 +42,8 @@ class SignatureStorage(models.Model):
     id = models.TextField(max_length=50, primary_key=True)
     docx = models.ForeignKey(DocxInit, on_delete=models.DO_NOTHING, verbose_name="模板")
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="填写人")
-    content = models.TextField(verbose_name="内容")
-    signature = models.TextField(verbose_name="签名", blank=True)
+    content = models.TextField(max_length=500, verbose_name="内容")
+    signature = models.TextField(max_length=10000, verbose_name="签名", blank=True)
     create_datetime = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
