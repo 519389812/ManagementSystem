6c592c91baab8fd9251aec343b89cb834f2de19d
519389812 <519389812>
2020-11-17 19:50:18 +0800

    17NOV20 25

--- document/static/document/js/app.js
+++ document/static/document/js/app.js
@@ -118,18 +118,20 @@ savePNGButton.addEventListener("click", function (event) {
     var cxck = getCookie("csrftoken");
     if (contentId!="") {
       var jsonData = JSON.stringify({
+        "data": dataURL,
         "docx_id": docxId,
         "content_id": contentId,
       });
-      xhr.open('post','http://teamwork.pythonanywhere.com/document/fill_signature/', false);
+      xhr.open('post','/document/fill_signature/', true);
     } else {
       var jsonData = JSON.stringify({
+        "data": dataURL,
         "docx_id": docxId,
         "signature_key": signatureKey,
       });
       xhr.open('post','/document/supervisor_signature/', true);
     }
-    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader("X-CSRFToken", cxck);
     xhr.send(jsonData);
     alert("签名成功！");
