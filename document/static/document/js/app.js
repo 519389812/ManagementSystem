eae6930188de7f912b11dff7eeec10e87a14479e
519389812 <519389812>
2020-11-17 18:01:44 +0800

    17NOV20 19

--- document/static/document/js/app.js
+++ document/static/document/js/app.js
@@ -117,13 +117,14 @@ savePNGButton.addEventListener("click", function (event) {
     var xhr=new XMLHttpRequest();
     var cxck = getCookie("csrftoken");
     if (contentId!="") {
-      var data='&docx_id='+docxId+'&content_id='+contentId;
+      var data='data='+dataURL+'&docx_id='+docxId+'&content_id='+contentId;
       xhr.open('post','/document/fill_signature/', true);
     } else {
-      var data='&docx_id='+docxId+'&signature_key='+signatureKey;
+      var data='data='+dataURL+'&docx_id='+docxId+'&signature_key='+signatureKey;
       xhr.open('post','/document/supervisor_signature/', true);
     }
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Content-Length', 4073);
     xhr.setRequestHeader("X-CSRFToken", cxck);
     xhr.send(data);
     alert("签名成功！");
