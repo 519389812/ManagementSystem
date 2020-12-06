15a16fcc60755ea46a6a9c80dfd1300184980a35
519389812 <519389812>
2020-12-06 14:40:16 +0800

    04 06DEC20

--- quickcheck/views.py
+++ quickcheck/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from user.views import check_authority
 
 
+@check_authority
 def quickcheck(request):
     return render(request, "quickcheck.html")
 
 
+@check_authority
 def outbound_limit(request):
     return render(request, "outbound_limit.html")
 
 
+@check_authority
 def outbound_limit_singapore(request):
     return render(request, "outbound_limit_singapore.html")
