fa378ae9eb7522686def7dba110b75673b3413ce
519389812 <519389812@qq.com>
2020-12-13 16:57:42 +0800

    02 13DEC20

--- user/admin.py
+++ user/admin.py
@@ -12,7 +12,7 @@ class CustomUserAdmin(UserAdmin):
         }),
         ('Important dates', {'fields': ('last_login', 'ip_address', 'date_joined')}),
     )
-    list_display = ('username', 'last_name', 'first_name', 'last_login', 'is_active')
+    list_display = ('username', 'last_name', 'first_name', 'last_login', 'ip_address', 'is_active')
     # readonly_fields = ('branch_id',)
 
     # def get_readonly_fields(self, request, obj=None):
