7dfb2da375f73a1177a9579d3448ae6cca559e50
519389812 <519389812@qq.com>
2020-12-17 13:42:30 +0800

    01 17DEC20

--- team/admin.py
+++ team/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from team.models import Team
 from user.models import User
+import json
 
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ("name", "get_related_name", )
+    list_display = ("name", "parent", "related_parent_name", )
     list_display_links = ("name", )
-    search_fields = ("name", "get_related_name", )
-    filter_horizontal = ("user__user", )
-    fields = ("name", "user__user",)
-    readonly_fields = ("related_parent", )
+    search_fields = ("name", )
+    fields = ("name", "parent", "related_parent", "related_parent_name", )
+    readonly_fields = ("related_parent", "related_parent_name", )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(related_parent=request.user.team.related_parent)
 
     def get_head_parent(self, obj):
         if obj.parent != "":
@@ -22,12 +29,11 @@ class TeamAdmin(admin.ModelAdmin):
         else:
             return obj
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        else:
-            return qs.filter(parent=request.user.branch_id)
+    def related_parent_name(self, obj):
+        related_parent_id_list = json.loads(obj.related_parent)
+        related_parent_name = '-->'.join([Team.objects.get(id=id).name for id in related_parent_id_list])
+        return related_parent_name
+    related_parent_name.short_description = "组织关系"
 
     def get_branch_name(self, obj):
         return obj.branch.branch_id
@@ -49,21 +55,34 @@ class TeamAdmin(admin.ModelAdmin):
             )
         return super(TeamAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
+    def get_related_parent(self, parent, related_parent):
+        while True:
+            try:
+                related_parent.insert(0, parent.id)
+                parent = parent.parent
+            except:
+                return related_parent
+
     def save_model(self, request, obj, form, change):
         if form.is_valid():
-            if change:
-                if obj.parent != form.cleaned_data["parent"]:
-                    related_parent = [obj.parent.id]
-                    if obj.parent:
-                        parent = obj.parent
-                        while True:
-                            try:
-                                related_parent.insert(0, parent.id)
-                                parent = parent.parent
-                            except:
-                                break
-                        obj.related_parent = str(related_parent)
+            if not change:
                 super().save_model(request, obj, form, change)
+                related_parent = [obj.id]
+                if form.cleaned_data["parent"]:
+                    parent = form.cleaned_data["parent"]
+                    related_parent = self.get_related_parent(parent, related_parent)
+                obj.related_parent = json.dumps(related_parent)
+            else:
+                if obj.parent != form.cleaned_data["parent"]:
+                    if not obj.parent:
+                        related_parent = [obj.id]
+                        parent = form.cleaned_data["parent"]
+                        related_parent = self.get_related_parent(parent, related_parent)
+                    elif not form.cleaned_data["parent"]:
+                        pass
+                    else:
+                        pass
+            super().save_model(request, obj, form, change)
 
 
 admin.site.register(Team, TeamAdmin)
